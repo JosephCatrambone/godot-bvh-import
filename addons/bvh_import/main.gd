591668d7f3923f360ad4e47e02fcfb86dd1c0ee0
Joseph Catrambone <jo.jcat@gmail.com>
2019-07-30 14:48:22 -0700

    Tweak naming.  bvh_import.gd -> plugin_setup.gd, because that's what it really does.  Fix bugs with missing [tool] header.  Create animations and select the armatures correctly.

--- addons/bvh_import/main.gd
+++ addons/bvh_import/main.gd
@@ -1,48 +1,65 @@
-extends VBoxContainer
+tool
+extends Container
 
-# DEV FLOW:
-# 1) Add a new animation.
-# 2) Add some elements to the new animation.
-# 3) Load a file from the select file dialog.
-# 4) Parse BVH and attach to bones.
+var editor_interface:EditorInterface
+var open_file_dialog:FileDialog
+var armature_name_input:LineEdit
+var animation_player_name_input:LineEdit
+var animation_name_input:LineEdit
+var import_button:Button
 
-# User flow:
-# Select animation_player.
-# (?) Select armature?  If we do this we might be able to remap animations.  Otherwise we have to rename bones.
-# Select BVH file from disk.
-# Edit the name mapping BVH bone -> armature bone. (Clear this if the bones don't match.  Otherwise, leave them.)
-# Hit import or reimport.
-
+# Bones in the armature may not have the same name as in the BVH.
 export(Dictionary) var bone_remapping:Dictionary = Dictionary()
-#var feature_order = [] # Index -> path to the bone attribute, like Armature:rotation.x or Armature:
-#var hierarchy = Dictionary()
 
-# BVH descriptor:
-# HIERARCHY
-# <hierarchy description>
-# MOTION
-# <motion description>
+func _ready():
+	open_file_dialog = get_node("FileDialog")
+	open_file_dialog.add_filter("*.bvh ; Biovision Hierarchy")
+	#open_file_dialog.mode = FileDialog.MODE_OPEN_FILE
+	open_file_dialog.connect("file_selected", self, "_finish_new_animation")
+	#get_editor_interface().get_base_control().add_child(open_file_dialog)
+	
+	armature_name_input = get_node("ArmatureNameInput")
+	animation_player_name_input = get_node("AnimationPlayerNameInput")
+	animation_name_input = get_node("AnimationNameInput")
+	import_button = get_node("ImportButton")
+	import_button.connect("pressed", self, "_create_new_animation")
+	print("Signals connected.")
 
 func _create_new_animation():
-	var animation_player_node_path = $AnimationPlayer/PlayerPath
-	var animation_player:AnimationPlayer = get_node(animation_player_node_path.text)
+	#open_file_dialog.show_modal(true)
+	open_file_dialog.popup_centered()
+
+func _finish_new_animation(file:String):
+	if file == null or file == "":
+		printerr("File is null")
+		return
+	var animation = load_bvh_filename(file)
+	
+	# Attach to the animation player.
+	#var editor_selection = editor_interface.get_selection()
+	#var selected_nodes = editor_selection.get_selected_nodes()
+	#if len(selected_nodes) == 0:
+	#	printerr("No nodes selected.  Please select the target animation player.")
+	#	return
+	var animation_player:AnimationPlayer = editor_interface.get_edited_scene_root().get_node(animation_player_name_input.text)
 	if animation_player == null:
-		printerr("Couldn't find animation player node with path ", animation_player_node_path)
+		printerr("AnimationPlayer is null.  Please ensure that the animation player to which you'd like to add is selected.")
 		return
 	
-	var animation_name_input = $AnimationNameInput
 	var animation_name = animation_name_input.text
 	if animation_player.has_animation(animation_name):
 		# TODO: Animation exists.  Prompt to overwrite.
 		animation_player.remove_animation(animation_name)
-	
-	var filename_input = $BVHFile/FilenameInput
-	var animation = load_bvh(filename_input.text)
-	
 	animation_player.add_animation(animation_name, animation)
 
-func load_bvh(filename:String) -> Animation:
-	var file = File.new()
+func load_bvh_file(file:File) -> Animation:
+	var plaintext = file.get_as_text()
+	return parse_bvh(plaintext)
+
+func load_bvh_filename(filename:String) -> Animation:
+	var file:File = File.new()
+	if !file.file_exists(filename):
+		printerr("Filename ", filename, " does not exist or cannot be accessed.")
 	file.open(filename, File.READ) # "user://some_data"
 	# file.store_string("lmao")
 	var plaintext = file.get_as_text()
@@ -51,23 +68,32 @@ func load_bvh(filename:String) -> Animation:
 func parse_bvh(fulltext:String) -> Animation:
 	# Split the fulltext by the elements from HIERARCHY to MOTION, and MOTION until the end of file.
 	var lines = fulltext.split("\n", false)
-	var hierarchy_start_line:int = -1
-	var motion_start_line:int = -1
-	var i:int = 0
+	var hierarchy_lines:Array = Array()
+	var motion_lines:Array = Array()
+	var hierarchy_section = false
+	var motion_section = false
 	
 	
 	for line in lines:
-		i += 1
+		# As written, we'll skip the 'hierarchy' and 'motion' lines.
 		if line.begins_with("HIERARCHY"):
-			hierarchy_start_line = i+1
+			hierarchy_section = true
+			motion_section = false
+			continue
 		elif line.begins_with("MOTION"):
-			motion_start_line = i+1
-			break
+			motion_section = true
+			hierarchy_section = false
+			continue
+		
+		if hierarchy_section:
+			hierarchy_lines.append(line)
+		elif motion_section:
+			motion_lines.append(line)
 	
-	var element_order_and_parent_map = parse_hierarchy(lines[hierarchy_start_line:motion_start_line-1])
+	var element_order_and_parent_map = parse_hierarchy(hierarchy_lines)
 	var element_order:Array = element_order_and_parent_map[0]
 	var parent_map:Dictionary = element_order_and_parent_map[1]
-	return parse_motion(element_order, parent_map, 60.0, lines[motion_start_line:])
+	return parse_motion(element_order, parent_map, 60.0, motion_lines)
 
 func parse_hierarchy(text:Array):# -> [Array, Dictionary]:
 	# Given the plaintext HIERARCHY from HIERARCHY until MOTION,
@@ -81,13 +107,24 @@ func parse_hierarchy(text:Array):# -> [Array, Dictionary]:
 	
 	return [element_ordering, parent]
 
-func parse_motion(element_order:Array, parent_map:Dictionary, fps:float, text:Array) -> Animation:
+func parse_motion(element_order:Array, parent_map:Dictionary, timestep:float, text:Array) -> Animation:
+	# Timestep = 1/fps
 	var animation:Animation = Animation.new()
 	# Set the length of the animation to match the BVH length.
-	#var animation = Animation.new()
-	#var track_index = animation.add_track(Animation.TYPE_VALUE)
-	#animation.track_set_path(track_index, "Enemy:position.x")
-	#animation.track_insert_key(track_index, 0.0, 0)
-	#animation.track_insert_key(track_index, 0.5, 100)
+	var element_track_index_map:Dictionary = Dictionary()
+	for i in range(len(element_order)):
+		#var track_index = animation.add_track(Animation.TYPE_VALUE)
+		var track_index = animation.add_track(Animation.TYPE_TRANSFORM)
+		element_track_index_map[i] = track_index
+	var step:int = 0
+	for line in text:
+		var values = line.split_floats(" ", false)
+		for i in range(len(element_order)):
+			var track_index = element_track_index_map[i]
+			var track_name = element_order[i]
+			animation.track_set_path(track_index, "Enemy:position.x")
+			animation.track_insert_key(track_index, step*timestep, values[i])
+			#animation.track_insert_key(track_index, 0.5, 100)
+		step += 1
 	
 	return animation
--- addons/bvh_import/plugin_setup.gd
+++ addons/bvh_import/plugin_setup.gd
@@ -1,20 +1,26 @@
 tool
 extends EditorPlugin
 
+#
+# Plugin boilerplate and housekeeping.
+#
+
 var dock
 
 func _enter_tree():
 	# Initialization of the plugin goes here
 	# Load the dock scene and instance it
 	dock = preload("res://addons/bvh_import/dock.tscn").instance()
+	dock.editor_interface = get_editor_interface()
 	
 	# Add the loaded scene to the docks
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
-	# Note that LEFT_UL means the left of the editor, upper-left dock
 	
 func _exit_tree():
-	# Clean-up of the plugin goes here
-	# Remove the dock
+	# Free memory.  Clean up signals.
+	if dock == null:
+		print("Dock is null.")
+	#dock.queue_free()
 	remove_control_from_docks(dock)
-	 # Erase the control from the memory
-	dock.free()
+
+
